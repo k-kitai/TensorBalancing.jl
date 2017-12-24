69dc9c67e448dfb160dfdd9fd11f4a8457091667
Koki Kitai <kitai.koki@gmail.com>
2017-12-24 15:33:41 +0900

    Add recursive version of balancing method

--- test/benchmark.jl
+++ test/benchmark.jl
@@ -5,11 +5,31 @@ using TensorBalancing
 TB = TensorBalancing
 
 Hessenberg(N) = cumsum(eye(N,N), 2)
+function Hessenberg_mod(N)
+    H = Hessenberg(N)
+    for i = 1:N-1
+        H[i+1, i] = 1.0
+    end
+    H
+end
 
+Ns = round.(Int32, exp.(linspace(log(100), log(1000), 10)))
+
+print("=== Newton Balancing ===\n")
 print("N\tCPU\n")
-for N = round.(Int32, exp.(linspace(log(100), log(8000), 10)))
+for N = Ns
     @printf "%4d\t" N
-    time1 = @belapsed TB.nBalancing($(Hessenberg(N)), 1.0e-6)
+    time1 = @belapsed TB.nBalancing($(Hessenberg_mod(N)), 1.0e-6)
+    print(time1, "\n")
+    #time2 = @belapsed TB.nBalancing_gpu($(Hessenberg(N)), 1.0e-6)
+    #print(time2, "\n")
+end
+
+print("=== Newton Balancing (recursive) ===\n")
+print("N\tCPU\n")
+for N = Ns
+    @printf "%4d\t" N
+    time1 = @belapsed TB.recBalancing($(Hessenberg_mod(N)), 1.0e-6)
     print(time1, "\n")
     #time2 = @belapsed TB.nBalancing_gpu($(Hessenberg(N)), 1.0e-6)
     #print(time2, "\n")
